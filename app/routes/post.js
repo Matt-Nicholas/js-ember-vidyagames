6cb8a90774f4302f2418e60607d22bf950a4ea75
Ben Vissotzky and Matt Nicholas <ben@>
2016-10-27 16:47:43 -0700

    finsihed

--- app/routes/post.js
+++ app/routes/post.js
@@ -25,8 +25,8 @@ export default Ember.Route.extend({
         var newComment = this.store.createRecord('comment', params);
         var post = params.post;
         post.get('comments').addObject(newComment);
-        newComment.save() .then(function() {
-        return post.save();
+        newComment.save().then(function() {
+          return post.save();
         });
       },
 
